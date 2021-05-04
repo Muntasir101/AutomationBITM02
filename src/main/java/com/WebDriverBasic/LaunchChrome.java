ce09d4fcee829870699573c467f9463c952957ba
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-05-04 12:12:07 +0600

    Browser Config

---
+++ src/main/java/com/WebDriverBasic/LaunchChrome.java
@@ -0,0 +1,20 @@
+package com.WebDriverBasic;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// launch Browser
+public class LaunchChrome {
+    public static void main(String[] args) {
+
+        //Implement WebDriver
+        WebDriver driver; // WebDriver=Selenium Interface; driver=reference variable
+
+        // Set browser Driver path
+        System.setProperty("webdriver.chrome.driver","F:\\Training\\PeopleNTech\\BITM Batch 2\\Files\\chromedriver.exe");
+
+        // launch Real Browser
+        driver=new ChromeDriver();
+    }
+}
---
+++ src/main/java/com/WebDriverBasic/LaunchFirefox.java
@@ -0,0 +1,19 @@
+package com.WebDriverBasic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+// launch Browser
+public class LaunchFirefox {
+    public static void main(String[] args) {
+
+        //Implement WebDriver
+        WebDriver driver; // WebDriver=Selenium Interface; driver=reference variable
+
+        // Set browser Driver path
+        System.setProperty("webdriver.gecko.driver","F:\\Training\\PeopleNTech\\BITM Batch 2\\Files\\geckodriver.exe");
+
+        // launch Real Browser
+        driver=new FirefoxDriver();
+    }
+}
