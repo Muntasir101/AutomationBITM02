98e0ac333494a850007c0cbc9ca304a6b55803db
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2021-05-23 12:20:31 +0600

    Test Base, Navigation, Wait, Scroll, Basic Auth

--- src/main/java/com/Rough/Test1.java
+++ src/main/java/com/Rough/Test1.java
@@ -2,6 +2,7 @@ package com.Rough;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+       // System.out.println("Hello Maven");
+        System.out.println(System.getProperty("user.dir"));
     }
 }
